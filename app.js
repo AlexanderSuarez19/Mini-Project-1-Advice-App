a4f005cdc2e8686b1e9dfd840c814fa70097869a
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-03-24 23:00:15 -0600

    User Story 7

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const adviceText = document.getElementById("advice-text");
 const adviceBtn = document.getElementById("advice-btn");
+const twitterBtn = document.getElementById("twitter-btn");
 
 function getNewAdvice() {
   fetch("https://api.adviceslip.com/advice")
@@ -11,8 +12,18 @@ function getNewAdvice() {
     .catch((error) => console.error(error));
 }
 
+function shareOnTwitter() {
+  const advice = adviceText.textContent;
+  const twitterUrl = `https://twitter.com/intent/tweet?url=${advice}`;
+  window.open(twitterUrl, "_blank");
+}
+
 adviceBtn.addEventListener("click", () => {
   getNewAdvice();
+});
+
+twitterBtn.addEventListener("click", () => {
+  shareOnTwitter();
 });
 
 getNewAdvice(); // Load advice when the app starts
