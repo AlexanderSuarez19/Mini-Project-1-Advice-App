f7d89a83a214eb451d7b93622fdd9fe22d26d6e8
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-03-24 23:12:26 -0600

    User Story 9

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const adviceText = document.getElementById("advice-text");
 const adviceBtn = document.getElementById("advice-btn");
 const twitterBtn = document.getElementById("twitter-btn");
+const container = document.querySelector(".container");
 
 function getNewAdvice() {
   fetch("https://api.adviceslip.com/advice")
@@ -8,6 +9,10 @@ function getNewAdvice() {
     .then((data) => {
       const advice = data.slip.advice;
       adviceText.textContent = advice;
+      const randomColor = `rgb(${getRandomInt(256)}, ${getRandomInt(
+        256
+      )}, ${getRandomInt(256)})`;
+      container.style.backgroundColor = randomColor;
     })
     .catch((error) => console.error(error));
 }
@@ -16,6 +21,10 @@ function shareOnTwitter() {
   const advice = adviceText.textContent;
   const twitterUrl = `https://twitter.com/intent/tweet?url=${advice}`;
   window.open(twitterUrl, "_blank");
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 adviceBtn.addEventListener("click", () => {
